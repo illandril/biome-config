337a312bc341074dc50f967d34b16ca4fb731136
Joe Spandrusyszyn <git@illandril.net>
2024-05-05 16:18:32 -0400

    Initial config files

---
+++ src/base.test.ts
@@ -0,0 +1,44 @@
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+import { expect, it } from 'vitest';
+
+const promiseExec = promisify(exec);
+
+it('test', async () => {
+  const promise = promiseExec(
+    'npx biome check --apply --config-path="biome.base.json" --stdin-file-path="example.ts"',
+    {
+      encoding: 'utf-8',
+      timeout: 5000,
+    },
+  );
+  promise.child.stdin?.write(
+    [
+      'import { it, expect } from "vitest";',
+      'import { delimiter } from "path";',
+      `it("delimiter is ':'", () => {`,
+      '    expect(',
+      '      delimiter',
+      '    ).toEqual(":")',
+      '})',
+    ].join('\n\r'),
+  );
+  promise.child.stdin?.end();
+  const { stdout, stderr } = await promise;
+  expect(stdout).toBe(
+    [
+      "import { expect, it } from 'vitest';",
+      '',
+      "import { delimiter } from 'path';",
+      '',
+      `it("delimiter is ':'", () => {`,
+      "  expect(delimiter).toEqual(':');",
+      '});',
+      '',
+    ].join('\n'),
+  );
+
+  expect(stderr).toContain('lint/style/useNodejsImportProtocol  FIXABLE');
+  expect(stderr).toContain(" - import·{·delimiter·}·from·'path';");
+  expect(stderr).toContain(" + import·{·delimiter·}·from·'node:path';");
+});
